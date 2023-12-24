00d12dc138c636435a50994a021fadbc2a1edba4
FutureDevKing <112193233+FutureDevKing@users.noreply.github.com>
2023-12-23 21:59:30 -0300

    new file:   aulas/desafio-felipao/index.html 	deleted:    aulas/desafio-felipao/index.js 	new file:   aulas/desafio-felipao/src/index.js

---
+++ aulas/desafio-felipao/src/index.js
@@ -0,0 +1,59 @@
+//menor que 1000 = ferro
+//entre 1001 e 2000 = Bronze
+//entre 2001 e 5000 = Prata
+//entre 6001 e 7000 = Ouro
+//entre 7001 e 8000 = Platina
+//entre 8001 e 9000 = Ascendente
+//entre 9001 e 10000 = Imortal
+//maior que 10001 = Radiante
+
+let xpHeroi = 6525
+let nomeHeroi = "Gabriel"
+let rankHeroi = ""
+
+// if( xpHeroi <= 1000){
+//     rankHeroi = "Ferro"
+// } else if( xpHeroi <= 2000){
+//     rankHeroi = "Bronze"
+// } else if( xpHeroi <= 5000){
+//     rankHeroi = "Prata"
+// }else if( xpHeroi <= 7000){
+//     rankHeroi = "Ouro"
+// }else if( xpHeroi <= 8000){
+//     rankHeroi = "Platina"
+// }else if( xpHeroi <= 9000){
+//     rankHeroi = "Ascendente"
+// }else if( xpHeroi <= 10000){
+//     rankHeroi = "Imortal"
+// } else {
+//     rankHeroi = "Radiante"
+// }
+
+switch (true){
+    case xpHeroi >= 1001 && xpHeroi <= 2000:
+        rankHeroi = "Bronze"
+        break
+    case xpHeroi >= 2001 && xpHeroi <= 6000:
+        rankHeroi = "Prata"
+        break
+    case xpHeroi >= 6001 && xpHeroi <= 7000:
+        rankHeroi = "Ouro"
+        break
+    case xpHeroi >= 7001 && xpHeroi <= 8000:
+        rankHeroi = "Platina"
+        break
+    case xpHeroi >= 8001 && xpHeroi <= 9000:
+        rankHeroi = "Ascendente"
+        break
+    case xpHeroi >= 9001 && xpHeroi <= 1000:
+        rankHeroi = "Imortal"
+        break
+    case xpHeroi > 10000:
+        rankHeroi = "Radiante"
+        break
+    default:
+        rankHeroi = "Ferro"  
+
+}
+
+console.log("O herói de nome " + nomeHeroi + " está no nível de " + rankHeroi)
